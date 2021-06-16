7544ba7988290e63c2b09f0d3ccca31c48b29216
Akarsh <akarshagarwal99@gmail.com>
2021-06-16 19:02:06 +0530

    new

--- myLambdaFunc/index.js
+++ myLambdaFunc/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from myLambdaFunc final'),
+        body: JSON.stringify('Hello froasfn,saf jsm myLambdaFunc final'),
     }
     return response
 }
